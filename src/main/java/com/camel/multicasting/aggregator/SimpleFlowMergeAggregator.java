2260b943103dbc2f569617caef5981c90acc4832
rahatalvishwajit <rahatalvishwajit@gmail.com>
2023-12-26 16:34:05 +0530

    Initial Setup for the App

---
+++ src/main/java/com/camel/multicasting/aggregator/SimpleFlowMergeAggregator.java
@@ -0,0 +1,27 @@
+package com.camel.multicasting.aggregator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
+
+public class SimpleFlowMergeAggregator implements AggregationStrategy {
+
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+
+        if (oldExchange == null) {
+            String data = newExchange.getIn().getBody(String.class);
+            List<String> aggregatedDataList = new ArrayList<>();
+            aggregatedDataList.add(data);
+            newExchange.getIn().setBody(aggregatedDataList);
+            return newExchange;
+        }
+
+        List<String> oldData = oldExchange.getIn().getBody(List.class);
+        oldData.add(newExchange.getIn().getBody(String.class));
+        oldExchange.getIn().setBody(oldData);
+        return oldExchange;
+    }
+}
---
+++ src/test/java/com/camel/multicasting/CamelMulticastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.multicasting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMulticastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
